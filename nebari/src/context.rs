4896dee8f0b4a5d2149a929b780aace2eeeb611b
Jonathan Johnson <jon@khonsulabs.com>
2021-10-01 17:10:36 -0700

    Fixing broken docs link

--- nebari/src/context.rs
+++ nebari/src/context.rs
@@ -5,7 +5,7 @@ use crate::{io::FileManager, ChunkCache, Vault};
 /// A shared environment for database operations.
 #[derive(Debug, Clone)]
 pub struct Context<M: FileManager> {
-    /// The file manager for the [`ManagedFile`](crate::ManagedFile) implementor.
+    /// The file manager for the [`ManagedFile`](crate::io::ManagedFile) implementor.
     pub file_manager: M,
     /// The optional vault in use.
     pub vault: Option<Arc<dyn Vault>>,

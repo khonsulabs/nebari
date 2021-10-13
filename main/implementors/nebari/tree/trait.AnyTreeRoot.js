aa1b3fcb839012fd02f07f56f57b31c6e5e905a7
kl-botsu <botsu@khonsulabs.com>
2021-10-13 14:46:06 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@8f01723de0e687c8b8efeb8fb747be176d59fc4f 🚀

---
+++ main/implementors/nebari/tree/trait.AnyTreeRoot.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["nebari"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

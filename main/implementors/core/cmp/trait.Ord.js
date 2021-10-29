4ff542f15346e0bd3d56e2e22cfaed7dda93f789
kl-botsu <botsu@khonsulabs.com>
2021-10-29 03:06:11 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@fcd16f91f9f5cb3f34fc2e7dfc2dca26a4c95c9a 🚀

--- main/implementors/core/cmp/trait.Ord.js
+++ main/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["nebari"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"nebari/struct.Buffer.html\" title=\"struct nebari::Buffer\">Buffer</a>&lt;'a&gt;","synthetic":false,"types":["nebari::buffer::Buffer"]}];
+implementors["nebari"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"nebari/struct.Buffer.html\" title=\"struct nebari::Buffer\">Buffer</a>&lt;'a&gt;","synthetic":false,"types":["nebari::buffer::Buffer"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

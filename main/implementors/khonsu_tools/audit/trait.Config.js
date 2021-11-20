f88b934ed30cec4110bc8104811c1852d223ee21
kl-botsu <botsu@khonsulabs.com>
2021-11-20 18:42:11 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@85ec464221309a7bfce3046d9fb3e5fb6fd43b17 🚀

---
+++ main/implementors/khonsu_tools/audit/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xtask"] = [{"text":"impl Config for <a class=\"struct\" href=\"xtask/struct.AuditConfig.html\" title=\"struct xtask::AuditConfig\">AuditConfig</a>","synthetic":false,"types":["xtask::AuditConfig"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

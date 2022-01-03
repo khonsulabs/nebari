04507e2df504232d7c799dda0c8156ae4ca74b8c
kl-botsu <botsu@khonsulabs.com>
2022-01-03 22:52:04 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@3717fbe353932dfc56747704bf177ce1b7254d62 🚀

--- main/implementors/structopt/trait.StructOpt.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["xtask"] = [{"text":"impl StructOpt for <a class=\"enum\" href=\"xtask/enum.Commands.html\" title=\"enum xtask::Commands\">Commands</a>","synthetic":false,"types":["xtask::Commands"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

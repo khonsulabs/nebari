662ab4a0cc17c6d23e12d73292eb6e206d8e9ecd
kl-botsu <botsu@khonsulabs.com>
2022-01-24 04:24:08 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@9c271c25dc42aed0e975805454894db15df2e964 🚀

--- main/implementors/core/marker/trait.Copy.js
+++ main/implementors/core/marker/trait.Copy.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["nebari"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"nebari/tree/enum.KeyEvaluation.html\" title=\"enum nebari::tree::KeyEvaluation\">KeyEvaluation</a>","synthetic":false,"types":["nebari::tree::KeyEvaluation"]}];
+implementors["nebari"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"nebari/tree/enum.KeyEvaluation.html\" title=\"enum nebari::tree::KeyEvaluation\">KeyEvaluation</a>","synthetic":false,"types":["nebari::tree::KeyEvaluation"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- main/source-files.js
+++ main/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["benchmarks"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["nebari"] = {"name":"","dirs":[{"name":"io","files":["fs.rs","memory.rs","mod.rs"]},{"name":"transaction","files":["log.rs","manager.rs","mod.rs","state.rs"]},{"name":"tree","files":["btree_entry.rs","by_id.rs","by_sequence.rs","interior.rs","key_entry.rs","mod.rs","modify.rs","root.rs","serialization.rs","state.rs","unversioned.rs","versioned.rs"]}],"files":["buffer.rs","chunk_cache.rs","context.rs","error.rs","lib.rs","roots.rs","vault.rs"]};
+sourcesIndex["nebari"] = {"name":"","dirs":[{"name":"io","files":["fs.rs","memory.rs","mod.rs"]},{"name":"transaction","files":["log.rs","manager.rs","mod.rs","state.rs"]},{"name":"tree","files":["btree_entry.rs","by_id.rs","by_sequence.rs","interior.rs","key_entry.rs","mod.rs","modify.rs","root.rs","serialization.rs","state.rs","unversioned.rs","versioned.rs"]}],"files":["chunk_cache.rs","context.rs","error.rs","lib.rs","roots.rs","vault.rs"]};
 sourcesIndex["xtask"] = {"name":"","files":["main.rs"]};
 createSourceSidebar();

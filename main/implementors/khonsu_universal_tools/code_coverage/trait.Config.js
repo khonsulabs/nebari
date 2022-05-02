193697575fdf0542a096a6805dbe463916be0bb2
kl-botsu <botsu@khonsulabs.com>
2022-05-02 02:16:39 +0000

    Deploying to gh-pages from @ khonsulabs/nebari@70ac91e32835117d594441e103c239ddef7a6ff6 🚀

---
+++ main/implementors/khonsu_universal_tools/code_coverage/trait.Config.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xtask"] = [{"text":"impl Config for <a class=\"enum\" href=\"xtask/enum.Config.html\" title=\"enum xtask::Config\">Config</a>","synthetic":false,"types":["xtask::Config"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
